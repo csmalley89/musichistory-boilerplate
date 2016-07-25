4c2e43af28e4f9f6ac0ab1fc025024fc381de967
Chris Smalley <chrism.smalley@gmail.com>
2016-07-25 18:14:32 -0500

    finished part 1 of assignment

---
+++ songs.js
@@ -0,0 +1,52 @@
+var music = [
+  {
+    song: "Bashful Beige Bunny",
+    artist: "alb/a.jpg",
+    album: "22.50",
+    genre: ""
+  },
+  {
+    song: "Grumpy Cat Plush",
+    artist: "alb/b.jpg",
+    album: "16.99",
+    genre: ""
+  },
+  {
+    song: "Pokemon Eevee 6-inch Plush",
+    artist: "alb/c.jpg",
+    album: "8.55",
+    genre: ""
+  },
+  {
+    song: "Zootopia Officer Judy Hopps",
+    artist: "alb/d.jpg",
+    album: "7.00",
+    genre: ""
+  },
+  {
+    song: "TY Beanie Boos - Slush the Husky",
+    artist: "alb/e.jpg",
+    album: "7.80",
+    genre: ""
+  },
+  {
+    song: "My Little Pony - Rainbow Dash",
+    artist: "alb/f.jpg",
+    album: "7.10",
+    genre: ""
+  },
+  {
+    song: "Stuffed Pusheen Plush Unicorn",
+    artist: "alb/g.jpg",
+    album: "14.38",
+    genre: ""
+  },
+  {
+    song: "Gravity Falls Waddles Plush",
+    artist: "alb/h.jpg",
+    album: "5.32",
+    genre: ""
+  }
+];
+
+
